df1cf1b211f5910e405ab7a17bc0628c766c99b0
Zachary Hills <codinfreakgf@gmail.com>
2024-08-05 22:33:36 -0400

    update

--- src/components/AnimatedSlide.vue
+++ src/components/AnimatedSlide.vue
@@ -1,40 +1,70 @@
 <template>
-  <div class="flex flex-column flex-auto flex-basis slide" @click="handleClick">
-      <slot name="header"></slot>
-      <div :class="showClass">
-      <slot name="content"></slot>
-      <slot name="footer"></slot>
+  <div class="flex flex-row">
+    <div class="pt-3">
+      <div
+        class="flex flex-column flex-auto flex-basis slide"
+        @click="handleClick"
+      >
+        <slot name="header"></slot>
+        <div :class="showClass">
+          <slot name="content"></slot>
+          <slot name="footer"></slot>
+        </div>
+      </div>
     </div>
+    <div class="text">2020</div>
+
+    <div class="dash flex flex-grow" :class="{ 'border-right': isTop }"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, withDefaults, defineProps } from 'vue';
+import { computed, ref, withDefaults, defineProps } from "vue";
 
 interface Props {
   canToggle?: boolean;
+  isTop?: boolean;
+  isBottom?: boolean;
 }
 
-const props = withDefaults(defineProps<Props>(), {canToggle: false});
+const props = withDefaults(defineProps<Props>(), {
+  canToggle: false,
+  isTop: false,
+  isBottom: false,
+});
 const isOpen = ref(false);
 const showClass = computed(() => {
-  return isOpen.value ? "" : "hidden"
-})
+  return isOpen.value ? "" : "hidden";
+});
 const handleClick = () => {
-  if(!props.canToggle) {
+  if (!props.canToggle) {
     return;
   }
   isOpen.value = !isOpen.value;
 };
-
 </script>
 <style scoped>
-  .slide {
-    background-color: #161B22;
-    padding: 1rem;
-    border-radius: 1rem;
-    margin-left: 5rem;
-    width: 42.5vw;
-    margin-bottom: 3rem;
-  }
+.slide {
+  background-color: #161b22;
+  border-radius: 1rem;
+  width: 42.5vw;
+  max-width: 1046px;
+}
+.border-right {
+  border-right: 1px solid #fff;
+  border-top-right-radius: 3px;
+}
+.dash {
+  margin-top: 0.5rem;
+  border-top: 1px solid #fff;
+  width: 1rem;
+  margin-left: 1rem;
+  -webkit-box-sizing: content-box; /* Safari/Chrome, other WebKit */
+  -moz-box-sizing: content-box; /* Firefox, other Gecko */
+  box-sizing: content-box; /* Opera/IE 8+ */
+}
+
+.text {
+  padding-left: 1rem;
+}
 </style>
--- src/components/LeadLagSlide.vue
+++ src/components/LeadLagSlide.vue
@@ -1,67 +1,66 @@
 <template>
-    <div>
-        <AnimatedSlide :canToggle="true">
-            <template #header>
-                <img src="/leadlag-header.png"  class="max-width-40" />
-            </template>
-            <template #content>
-                <div class="blog-post-wrapper">
-                    <h2>Consilium Maps</h2>
-                    <p class="text">
-                      Consilium Maps is a data exploratory tool based on the lead-lag
-                      metric, which measures the offset between research outputs of
-                      different countries. For example, a search for "spinach" shows how
-                      each country’s research output compares to Canada’s. This tool
-                      uses data from
-                      <a href="https://www.dimensions.ai/">Dimensions.AI</a>, which
-                      includes over 108 million papers with metadata on authors,
-                      affiliations, funding, citations, and patents.
-                    </p>
-                    <p class="text">
-                      To manage the vast dataset, we use a progressive loader that
-                      allows interaction with partially loaded data. The scrubber
-                      represents Canada’s data and can be resized to select between 3
-                      and 10 years. This helps users gauge areas of interest in Canada’s
-                      research output over time.
-                      <img
-                        src="/leadlag-scrubber.png"
-                        alt="Scrubber interface displaying a portion of Canada's research data. Users can interact with and resize this tool to view different time spans of data, which helps in analyzing trends and areas of interest."
-                      />
-                    </p>
-                    <p class="text">
-                      Event graphs highlight regions with the most significant lead,
-                      lag, and participation from various countries. These graphs
-                      provide a clear visualization of global research dynamics and help
-                      identify key trends and areas with notable research activity.
-                      <img
-                        src="/leadlag-events.png"
-                        alt="Event graphs showcasing regions with the highest lead and lag in research output. The graphs also highlight regions with the most participating countries, providing an overview of global research trends."
-                      />
-                    </p>
-                    <p class="text">
-                      After selecting a timespan, users can view individual
-                      institutions' outputs relative to Canada. Nodes on the map
-                      indicate whether an institution is leading or lagging in research
-                      output compared to Canada. Trend lines on these nodes show
-                      significant changes in output, with an upward trend indicating a
-                      5% or more increase.
-                      <img
-                        src="/leadlag-nodes.png"
-                        alt="Map showing individual institutions with nodes indicating their research output relative to Canada. Each node’s color represents lead or lag status, and the trend line displays changes in output over the selected timespan."
-                      />
-                    </p>
-                    <p class="text">
-                      Consilium Maps aims to provide stakeholders with valuable insights
-                      into institutional performance on a global scale, helping to
-                      assess how well different institutions and countries are
-                      performing in various research areas.
-                    </p>
-                </div>
-        </template>
-        </AnimatedSlide>
-    </div>
+  <div>
+    <AnimatedSlide :canToggle="true" isTop>
+      <template #header>
+        <img src="/leadlag-header.png" class="max-width-40" />
+      </template>
+      <template #content>
+        <div class="blog-post-wrapper">
+          <h2>Consilium Maps</h2>
+          <p class="text">
+            Consilium Maps is a data exploratory tool based on the lead-lag
+            metric, which measures the offset between research outputs of
+            different countries. For example, a search for "spinach" shows how
+            each country’s research output compares to Canada’s. This tool uses
+            data from
+            <a href="https://www.dimensions.ai/">Dimensions.AI</a>, which
+            includes over 108 million papers with metadata on authors,
+            affiliations, funding, citations, and patents.
+          </p>
+          <p class="text">
+            To manage the vast dataset, we use a progressive loader that allows
+            interaction with partially loaded data. The scrubber represents
+            Canada’s data and can be resized to select between 3 and 10 years.
+            This helps users gauge areas of interest in Canada’s research output
+            over time.
+            <img
+              src="/leadlag-scrubber.png"
+              alt="Scrubber interface displaying a portion of Canada's research data. Users can interact with and resize this tool to view different time spans of data, which helps in analyzing trends and areas of interest."
+            />
+          </p>
+          <p class="text">
+            Event graphs highlight regions with the most significant lead, lag,
+            and participation from various countries. These graphs provide a
+            clear visualization of global research dynamics and help identify
+            key trends and areas with notable research activity.
+            <img
+              src="/leadlag-events.png"
+              alt="Event graphs showcasing regions with the highest lead and lag in research output. The graphs also highlight regions with the most participating countries, providing an overview of global research trends."
+            />
+          </p>
+          <p class="text">
+            After selecting a timespan, users can view individual institutions'
+            outputs relative to Canada. Nodes on the map indicate whether an
+            institution is leading or lagging in research output compared to
+            Canada. Trend lines on these nodes show significant changes in
+            output, with an upward trend indicating a 5% or more increase.
+            <img
+              src="/leadlag-nodes.png"
+              alt="Map showing individual institutions with nodes indicating their research output relative to Canada. Each node’s color represents lead or lag status, and the trend line displays changes in output over the selected timespan."
+            />
+          </p>
+          <p class="text">
+            Consilium Maps aims to provide stakeholders with valuable insights
+            into institutional performance on a global scale, helping to assess
+            how well different institutions and countries are performing in
+            various research areas.
+          </p>
+        </div>
+      </template>
+    </AnimatedSlide>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import AnimatedSlide from '@/components/AnimatedSlide.vue';
+import AnimatedSlide from "@/components/AnimatedSlide.vue";
 </script>
